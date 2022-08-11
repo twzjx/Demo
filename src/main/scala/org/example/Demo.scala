ea89a631b0ebfa94a38a0ea9135b6662e6a57706
Jiaxing Zhou <jiaxing.zhou1@thoughtworks.com>
2022-08-11 11:12:20 +0800

    updated pom.xml

---
+++ src/main/scala/org/example/Demo.scala
@@ -0,0 +1,14 @@
+package org.example
+
+import com.apple.eawt.Application
+
+/**
+ * @author jiaxing zhou
+ * @version 1.0
+ * @Date 2022/8/11
+ */
+object Demo {
+    def main(args:Array[String]):Unit={
+        println("hello world")
+    }
+}
