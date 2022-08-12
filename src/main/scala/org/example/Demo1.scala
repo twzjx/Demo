75c9d0d7aa4c8ec68858a36bed6b6ba66094cb64
jiaxing.zhou1 <jiaxing.zhou1@thoughtworks.com>
2022-08-12 09:40:32 +0800

    refactor demo1

--- src/main/scala/org/example/Demo1.scala
+++ src/main/scala/org/example/Demo1.scala
@@ -15,35 +15,37 @@ object Demo1 {
     val FILE_PATH = "movies.csv"
 
     def main(args: Array[String]): Unit = {
-        val map = createSparkAndDataFrame()
-        rankByYearGross(map)
-        rankByMovieType(map)
+        implicit val spark = createSparkSession()
+        val df = createDataFrame()
+        val rankedDF = rankByYearGross(df)
+        val rankedDF1 = rankByMovieType(df)
     }
 
-    def createSparkAndDataFrame() = {
-        val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Demo1")
-        val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+    def createDataFrame()(implicit spark: SparkSession) = {
         var df = spark.read.options(Map("header" -> "true", "inferSchema" -> "true", "delimiter" -> ",")).csv(FILE_PATH)
         df = df.withColumn("Worldwide_Gross", regexp_replace(col("Worldwide_Gross"), "\\$", ""))
-        val map = Map("spark" -> spark, "df" -> df)
-        map
+        df
     }
 
-    def rankByYearGross(map: Map[String, Serializable]) = {
-        val df = map("df").asInstanceOf[DataFrame]
-        val spark = map("spark").asInstanceOf[SparkSession]
+    def createSparkSession(): SparkSession = {
+        val sparkConf = new SparkConf().setMaster("local[*]").setAppName("Demo1")
+        val spark = SparkSession.builder().config(sparkConf).getOrCreate()
+        spark
+    }
+
+    def rankByYearGross(df: DataFrame) = {
+        val spark = df.sparkSession
         import spark.implicits._
         df.show()
         var result = df.select($"year", $"Worldwide_Gross").groupBy("year")
             .agg(sum("Worldwide_Gross").as("sum_gross"))
             .orderBy(desc("sum_gross"))
         result = result.withColumn("sum_gross", round(col("sum_gross"), 2))
-        result.show()
+        result
     }
 
-    def rankByMovieType(map: Map[String, Serializable]): Unit = {
-        val df = map("df").asInstanceOf[DataFrame]
-        val spark = map("spark").asInstanceOf[SparkSession]
+    def rankByMovieType(df: DataFrame) = {
+        val spark = df.sparkSession
         import spark.implicits._
         df.show()
         var result = df.select($"year", $"Worldwide_Gross", $"Genre")
@@ -51,6 +53,6 @@ object Demo1 {
             .agg(sum("Worldwide_Gross").as("sum_gross"))
             .orderBy(desc("sum_gross"))
         result = result.withColumn("sum_gross", round(col("sum_gross"), 2))
-        result.show()
+        result
     }
 }
